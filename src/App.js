673d720e3607b20e3c22f0f0111a3b32f749a0a8
Adithya kamath <kamathadithya12@gmail.com>
2024-01-02 19:34:40 +0530

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Hero from "./Components/Hero";
+import Header from "./Components/Header";
+import Skills from "./Components/Skills";
+import Experiance from "./Components/Experiance";
+import ContactForm from "./Components/ContactForm";
 
 function App() {
+  const createBrowserRoute = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div className="bg-primary-bg text-primary-text w-full h-full ">
+          <Header />
+          <Hero />
+          <Skills />
+          <Experiance />
+          <ContactForm />
+        </div>
+      ),
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <RouterProvider router={createBrowserRoute}></RouterProvider>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      "primary-text": "#242423",
+      "primary-bg": "#dee2e6",
+      "primary-dark-text": "#343a40",
+      "primary-dark-bg": "#212529",
+      "primary-orange": "#ffba08",
+      "primary-yellow": "#fca311",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
